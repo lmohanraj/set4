a6e3bfa46467898e43247e6c614c5a766f41800e
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-01-27 16:01:30 +0530

    Create nooflinesinpara.c

---
+++ nooflinesinpara.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() 
+{ 
+char para[1000]; 
+int charac, lines; 
+printf("Enter your a paragraph\n: "); 
+gets(para); 
+charac = strlen(para);
+lines = 0;
+for (int i=0; i<charac; i++)
+{
+if (para[i]=='.')
+{
+lines++;
+}
+}
+printf("\n\nTotal lines: %d",lines); 
+getch();
+return 0; 
+} 
